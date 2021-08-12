61c5e45246e63c84b00cd1c4793487ce01ac9b84
Angélica Albano <31373435+AngelicaAlbano@users.noreply.github.com>
2021-08-12 09:40:08 -0300

    Update ex02.py
    refact: Capitalize no nome do mês

--- Semana02/ex02.py
+++ Semana02/ex02.py
@@ -10,7 +10,7 @@ data = input('Digite uma data no formato dd/mm/aaaa: ')
 def data_extenso(data):
     dd, mm, yyyy = data.split('/')
     dia = dd
-    mes = calendar.month_name[int(mm)]
+    mes = calendar.month_name[int(mm)].capitalize()
     ano = yyyy
     nova_data = dia+' de '+mes+' de '+ano
     return nova_data
